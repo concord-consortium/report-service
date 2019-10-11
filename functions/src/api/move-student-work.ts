0dc7c9dfe62ab3934567b99d7b8c6cc5b476d23a
Unknown <emcelroy@concord.org>
2019-10-11 12:04:09 -0400

    Add makeSourceKey. Fix query params. Send 200 response on update.

--- functions/src/api/move-student-work.ts
+++ functions/src/api/move-student-work.ts
@@ -1,38 +1,60 @@
+import admin from "firebase-admin"
 import { Request, Response } from "express"
-import { getDoc, getRunPath, getAnswerPath } from "./helpers/paths"
 
-export const ParseStudentWork = (body) => {
+// This matches the make_source_key method in LARA's report_service.rb
+export const makeSourceKey = (toolId: string) => {
+  return toolId.replace(/http[|s]?:\/\/([^\/]+)/, "$1")
+}
+
+export default (req: Request, res: Response) => {
+  console.log('Move started.')
+
+  const body = req.body
   const classInfoUrl = body.class_info_url
-  const contextId = body.context_id
+  const currentContextId = body.current_context_id
+  const newContextId = body.context_id
   const platformId = body.platform_id
   const platformUserId = body.platform_user_id
   const assignments = body.assignments
 
-  return {classInfoUrl: classInfoUrl, contextID: contextId, platformId: platformId, platformUserId: platformUserId, assignments: assignments}
-}
+  console.log('Variables set from submitted JSON.')
 
-export default (req: Request, res: Response) => {
+  assignments.forEach((assignment:any) => {
+    console.log('Iteration through assignments begun.')
+    const toolId = makeSourceKey(assignment.tool_id)
+    console.log('Tool ID set: ' + toolId)
+    const data = {class_info_url: classInfoUrl, context_id: newContextId, platform_id: platformId, resource_link_id: assignment.new_resource_link_id}
+    console.log('Data set: ' + data)
 
-  const {classInfoUrl, contextId, platformId, platformUserId, assignments} = ParseStudentWork(req.body)
-
-  assignments.forEach((assignment, i) => {
-    let toolId = assignment.tool_id
-    let data = {class_info_url: classInfoUrl, context_id: contextId, platform_id: platformId, resource_link_id: assignment.new_resource_link_id}
-
-    let answersQuery = db.collection(`sources/${toolId}/answers`)
-      .where("context_id", "==", contextId)
+    admin.firestore().collection(`sources/${toolId}/answers`)
+      .where("context_id", "==", currentContextId)
       .where("platform_id", "==", platformId)
       .where("platform_user_id", "==", platformUserId.toString())
-      .where("resource_link_id", "==", assignment.old_resource_link_id.toString())
+      .where("resource_link_id", "==", assignment.current_resource_link_id.toString())
       .get()
-      .then((QuerySnapshot) => {
-        QuerySnapshot.forEach((document) => {
-          document.ref().set(data, {merge: true})
-        })
+      .then((querySnapshot) => {
+        console.log('Number of matching docs in querySnapshot: ' + querySnapshot.size)
+        if (querySnapshot.size > 0) {
+          querySnapshot.forEach((document) => {
+            const ref = document.ref
+            ref.set(data, {merge: true})
+               .then( (success) => {
+                 res.status(200).send("")
+                 console.log('Work updated.')
+               })
+               .catch( (e) => {
+                 console.error(e)
+               })
+
+          })
+        } else {
+          console.log('No matching docs for context_id=' + currentContextId + ' & platform_id=' + platformId + ' & platform_user_id=' + platformUserId + ' & resource_link_id=' + assignment.current_resource_link_id.toString())
+        }
       })
-      .catch((e) => {
+      .catch( (e) => {
         console.error(e);
         res.error(500, {error: e})
       })
-  }
+  })
+  console.log('End.')
 }
