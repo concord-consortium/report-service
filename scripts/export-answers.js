48ce57a94c2334f865ed7d65791c51ea653e8383
Doug Martin <dmartin@concord.org>
2020-07-02 15:03:45 -0400

    Change answer upload to use partitioned parquet files [#173607905]

--- scripts/export-answers.js
+++ scripts/export-answers.js
@@ -1,12 +1,23 @@
 #!/usr/bin/env node
 
 const BUCKET = "concordqa-report-data"
-const DIRECTORY = "answers"
+const DIRECTORY = "partitioned-answers"
+const BATCH_SIZE = 100;
 
 var AWS = require('aws-sdk');
 const {Firestore} = require('@google-cloud/firestore');
 const fs = require('fs');
 const path = require('path');
+
+const zlib = require('zlib');
+const util = require('util');
+const gzip = util.promisify(zlib.gzip);
+
+const access = util.promisify(fs.access);
+const unlink = util.promisify(fs.unlink);
+const readFile = util.promisify(fs.readFile);
+
+const parquet = require('parquetjs');
 
 const awsConfig = "./config.json"
 if (!fs.existsSync(awsConfig)) {
@@ -23,7 +34,7 @@ if (!fs.existsSync(process.env.GOOGLE_APPLICATION_CREDENTIALS)) {
   process.exit(1);
 }
 
-const outputPath = path.join(__dirname, "answers");
+const outputPath = path.join(__dirname, "uploads");
 if (!fs.existsSync(outputPath)) {
   fs.mkdirSync(outputPath)
 }
@@ -31,31 +42,117 @@ if (!fs.existsSync(outputPath)) {
 // Create a new client
 const firestore = new Firestore();
 
-const batchSize = 100;
 let query = firestore.collection('sources/authoring.concord.org/answers');
-let masterQuery = query.limit(batchSize)
-  .orderBy('id')
+let masterQuery = query.limit(BATCH_SIZE)
+  .orderBy('run_key')
 
 let count = 0;
 let lastDoc = null;
 let batchCount = 0;
 let batchIndex = 0;
+let lastRunKey = undefined;
+let waitingForUploadsToFinish = false;
+const answerHash = {};
+const uploadPromises = {};
+
+const schema = new parquet.ParquetSchema({
+  submitted: { type: 'BOOLEAN', optional: true },
+  run_key: { type: 'UTF8' },
+  platform_user_id: { type: 'UTF8', optional: true },
+  id: { type: 'UTF8' },
+  context_id: { type: 'UTF8', optional: true },
+  class_info_url: { type: 'UTF8', optional: true },
+  platform_id: { type: 'UTF8', optional: true },
+  resource_link_id: { type: 'UTF8', optional: true },
+  type: { type: 'UTF8' },
+  question_id: { type: 'UTF8' },
+  source_key: { type: 'UTF8' },
+  question_type: { type: 'UTF8' },
+  tool_user_id: { type: 'UTF8' },
+  answer: { type: 'UTF8' },
+  resource_url: { type: 'UTF8' },
+  remote_endpoint: { type: 'UTF8', optional: true },
+  created: { type: 'UTF8' },
+  tool_id: { type: 'UTF8' },
+  version: { type: 'UTF8' },
+});
+
+function uploadAnswers(runKey) {
+  if (!answerHash[runKey] || (answerHash[runKey].length === 0)) {
+    return;
+  }
+
+  const answers = answerHash[runKey];
+  const answer = answers[0];
+  const filename = path.join(outputPath, `${answer.run_key}.parquet`);
+  const folder = answer.resource_url.replace(/[^a-z0-9]/g, "-");
+  const key = `${DIRECTORY}/${folder}/${runKey}.parquet.gz`;
+
+  if (uploadPromises[runKey]) {
+    console.error("Already uploading", runKey)
+    return;
+  }
+
+  const deleteFile = async () => {
+    return access(filename).then(() => unlink(filename)).catch(() => undefined);
+  }
+
+  uploadPromises[runKey] = new Promise(async (resolve) => {
+    try {
+      // parquetjs can't write to buffers
+      await deleteFile();
+      var writer = await parquet.ParquetWriter.openFile(schema, filename);
+      for (let i = 0; i < answers.length; i++) {
+        answers[i].answer = JSON.stringify(answers[i].answer)
+        await writer.appendRow(answers[i]);
+      }
+      await writer.close();
+
+      const contents = await readFile(filename)
+      const body = await gzip(contents)
+
+      // write file to s3
+      // console.log("uploading", key)
+      await s3.putObject({
+        Bucket: BUCKET,
+        Key: key,
+        Body: body,
+        ContentType: 'application/json',
+        ContentEncoding: "gzip"
+      }, function (err) {
+        if (err) {
+          console.error(`${documentSnapshot.id}: ${err.toString()}`)
+        }
+      }).promise();
+    } catch (err) {
+      console.error(err);
+    } finally {
+      await deleteFile();
+    }
+
+    // while running keep the upload promises object as small as possible
+    if (!waitingForUploadsToFinish) {
+      delete uploadPromises[runKey];
+    }
+
+    // always resolve
+    resolve();
+  });
+
+  // console.log("starting", key)
+  // uploadPromises[runKey].then(() => console.log("done", key))
+  Promise.resolve(uploadPromises[runKey])
+}
 
 function countBatch(query) {
   query.stream().on('data', (documentSnapshot) => {
-    let data = documentSnapshot.data();
-    // write file to s3
-    s3.putObject({
-      Bucket: BUCKET,
-      Key: `${DIRECTORY}/${documentSnapshot.id}`,
-      // ACL: 'public-read',
-      Body: JSON.stringify(data),
-      ContentType: 'application/json',
-    }, function (err) {
-      if (err) {
-        console.error(`${documentSnapshot.id}: ${err.toString()}`)
-      }
-    });
+    const answer = documentSnapshot.data();
+    answerHash[answer.run_key] = answerHash[answer.run_key] || [];
+    answerHash[answer.run_key].push(answer);
+    if (lastRunKey && (answer.run_key !== lastRunKey)) {
+      uploadAnswers(lastRunKey)
+    }
+    lastRunKey = answer.run_key;
     lastDoc = documentSnapshot;
     ++count;
     ++batchCount;
@@ -63,17 +160,33 @@ function countBatch(query) {
   }).on('end', onEnd);
 }
 
+function finish() {
+  console.log(`Finished: ${batchIndex}, uploads: ${Object.keys(uploadPromises).length}, last_id: ${lastDoc.id}, memory: ${JSON.stringify(process.memoryUsage())}`);
+  waitingForUploadsToFinish = true;
+  uploadAnswers(lastRunKey);
+  const promises = Object.values(uploadPromises);
+  console.log(`Waiting for ${promises.length} uploads to finish...`);
+  Promise.allSettled(promises).then(() => console.log("Done waiting!"))
+}
+
 function onEnd() {
-  if(batchCount == batchSize) {
+  if (batchIndex === 10) {
+    finish();
+  }
+  else if (batchCount == BATCH_SIZE) {
     batchCount = 0;
-    console.log(`Ended batch: ${batchIndex} last_id: ${lastDoc.id} ${JSON.stringify(process.memoryUsage())}`);
+    console.log(`Ended batch: ${batchIndex}, uploads: ${Object.keys(uploadPromises).length}, last_id: ${lastDoc.id}, memory: ${JSON.stringify(process.memoryUsage())}`);
     ++batchIndex;
     // This might be bad because it might be growing the stack
     // but at least it isn't closing around anything extra
-    countBatch(masterQuery.startAfter(lastDoc))
+    setTimeout(() => {
+      countBatch(masterQuery.startAfter(lastDoc))
+    }, 1)
   } else {
+    finish();
     console.log(`Total count is ${count}`);
   }
 }
 
+console.log("Starting query with batch size of", BATCH_SIZE);
 countBatch(masterQuery);
