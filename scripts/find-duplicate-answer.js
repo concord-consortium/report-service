7a8b6efd86eb8c297547300e846c46ef5f13e563
eireland <eireland@concord.org>
2021-04-28 14:57:17 -0700

    checks questions with multiple answers have resources associated with them

--- scripts/find-duplicate-answer.js
+++ scripts/find-duplicate-answer.js
@@ -8,37 +8,78 @@ if (!fs.existsSync(process.env.GOOGLE_APPLICATION_CREDENTIALS)) {
   process.exit(1);
 }
 
+const getQuestionKey = (questionUser) => {
+  return question = questionUser.split("@")[1]
+}
+
 const firestore = new Firestore();
 const answersRef = firestore.collection(`sources/authoring.staging.concord.org/answers`)
+const resourceRef = firestore.collection(`sources/authoring.staging.concord.org/resources`)
 let answerArr = [];
-let multipleAnswers = [];
+let questionArr = [];
+let resourceArr = [];
 let questionUserMap = new Map;
 
-answersRef.get().then(answerSnapshot => {
+answersRef.orderBy("platform_user_id", "asc").orderBy("question_id", "asc").get().then(answerSnapshot => {
+  let prevUserId = "";
+  let prevQuestionId = "";
   answerSnapshot.forEach(document => {
     if (document.exists) {
       let data = document.data();
-      let dataKey = (data.platform_user_id && data.question_id)? `${data.platform_user_id}@${data.question_id}`: undefined
-      if (dataKey && questionUserMap.has(dataKey)) {
-        answerArr = questionUserMap.get(dataKey)
+      let userId = data.platform_user_id;
+      let questionId = data.question_id;
+      if ((userId === prevUserId) && (questionId === prevQuestionId)) {
+        let dataKey = (userId && questionId) ? `${userId}@${questionId}` : undefined
+        if (dataKey && questionUserMap.has(dataKey)) {
+          answerArr = questionUserMap.get(dataKey)
 
-        if (!answerArr.includes(data.id)) {
-          answerArr.push(data.id)
+          if (!answerArr.includes(data.id)) {
+            answerArr.push(data.id)
+          }
+        } else {
+          questionUserMap.set(dataKey, [data.id]);
         }
-      } else {
-        questionUserMap.set(dataKey, [data.id]);
       }
+      prevUserId = userId;
+      prevQuestionId = questionId;
     } else {
-      console.error("No resource found with that id!")
+      console.error("No answer found with that id!")
     }
   })
-  for (let [questionUser, answer] of questionUserMap) {
-    if (answer.length > 1 ) {
-      multipleAnswers.push(questionUser)
+
+  for (let [questionUser] of questionUserMap) {
+    let questionId = questionUser && getQuestionKey(questionUser)
+    if (!questionArr.includes(questionId)) {
+      questionArr.push(questionId)
     }
   }
-  var file = fs.createWriteStream("multipleAnswerStudents.txt")
-  multipleAnswers.forEach(item=>{file.write(item + ", \n")})
-  file.end()
+
+  resourceRef.get().then(resourceSnapshot => {
+    resourceSnapshot.forEach(resource => {
+      if (resource.exists) {
+        let activity = resource.data()
+        let pages = activity.children
+        pages.forEach(page => {
+          let sections = page.children
+          sections.forEach(section => {
+            let parts = section.children
+            parts.forEach(part => {
+              if (!part.id.includes("page")) {
+                if (questionArr.includes(part.id) && !resourceArr.includes(part.id)) {
+                  resourceArr.push(resource)
+                }
+              }
+            })
+          })
+        })
+      }
+    })
+    console.log(resourceArr.length)
+  })
+
+  // var file = fs.createWriteStream("multipleAnswerStudents.txt")
+  // multipleAnswers.forEach(item=>{file.write(item + ", \n")})
+  // file.end()
+
 })
 
