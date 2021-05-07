fe0f4c6953b8fb9a8d14f976315f1ea3b4521e9e
Sam Fentress <sfentress@concord.org>
2021-05-07 08:41:16 -0400

    Replace run_key with LTI identifiers for logged-in users
    When we have a logged-in user, we expect to also have a platform_id, a
    resource_link_id and a platform_user_id. These three identifiers together
    uniquely identify a learner-assignment pair.
    
    This updates the sync_doc and answer-finding code to support these.
    
    This also changes the storage location of the Parquet files in S3,
    putting them in nested folders that allow each learner-assignment to
    create a unique file. For anonymous users, this goes in a new
    "anonymous" folder.

--- functions/src/shared/s3-answers.ts
+++ functions/src/shared/s3-answers.ts
@@ -1,10 +1,19 @@
 const parquet = require('parquetjs');
 
-export interface AnswerData {
+export interface AnswerMetadata {
+  resource_url: string;
+  // logged-in user
+  platform_id?: string;
+  resource_link_id?: string;
+  platform_user_id?: string;
+  // anonymous user
+  run_key?: string;
+}
+
+export interface AnswerData extends AnswerMetadata {
   // leaving out everything but the resource_url and run_key which is what we care about
   answer?: any;
-  resource_url: string;
-  run_key?: string;
+
 }
 
 export const schema = new parquet.ParquetSchema({
@@ -29,23 +38,66 @@ export const schema = new parquet.ParquetSchema({
   version: { type: 'UTF8' },
 });
 
-export const parquetInfo = (directory: string, answer?: AnswerData | null, _runKey?: string, _resourceUrl?: string) => {
-  let runKey, resourceUrl;
-  if (answer) {
-    runKey = answer.run_key;
-    resourceUrl = answer.resource_url;
-  } else {
-    runKey = _runKey;
-    resourceUrl = _resourceUrl;
-  }
-  if (!runKey || !resourceUrl) {
-    throw Error(`Cannot create filename for ${runKey}`);
-  }
+// replaces everything but alphanumeric chars with -
+const escapeUrl = (url: string) => url.replace(/[^a-z0-9]/g, "-")
 
-  const filename = `${runKey}.parquet`;
-  const folder = resourceUrl.replace(/[^a-z0-9]/g, "-");
-  return {
-    filename,
-    key: `${directory}/${folder}/${filename}`
+/**
+ * Returns the filename and directory structure that the parquet file should end up in, given an answer.
+ *
+ * For a logged-in user launching from the portal (LARA or Activity Player):
+ *  answers/[escaped_resource_id]/[platform_id]/[resource_link_id]/[platform_user_id].parquet
+ *
+ * For an anonymous user (LARA or AP):
+ *  answers/[escaped_resource_id]/anonymous/no-resource-link/[run-key].parquet
+ *
+ * @param directory Top-level directory in S3 bucket (partitioned-answers)
+ * @param answer Answer or metadata-only info
+ */
+export const parquetInfo = (directory: string, answer: AnswerMetadata) => {
+  const escaped_resource_id = escapeUrl(answer.resource_url);
+  let filename: string;
+  if (escaped_resource_id && answer.platform_id && answer.resource_link_id && answer.platform_user_id) {
+    filename = `${answer.platform_user_id}.parquet`;
+    return {
+      filename,
+      key: `${directory}/${escaped_resource_id}/${escapeUrl(answer.platform_id)}/${answer.resource_link_id}/${filename}`
+    };
+  } else if (answer.run_key) {
+    filename = `${answer.run_key}.parquet`;
+    return {
+      filename,
+      key: `${directory}/${escaped_resource_id}/anonymous/no-resource-link/${filename}`
+    };
+  } else {
+    throw Error(`Cannot create filename`);
   }
+}
+
+// returns just the part needed to identify the answer uniquely
+// urls don't need to be escaped because the document name will be hashed
+export const getSyncDocId = (answer: AnswerData) => {
+  if (answer.platform_id && answer.resource_link_id && answer.platform_user_id) {
+    return `${answer.platform_id}_${answer.resource_url}_${answer.platform_user_id}`;
+  } else if (answer.run_key) {
+    return answer.run_key;
+  }
+  return null;
+}
+
+// returns just the part needed to identify the answer uniquely
+export const getAnswerMetadata = (answer: AnswerData) => {
+  if (answer.resource_url && answer.platform_id && answer.resource_link_id && answer.platform_user_id) {
+    return {
+      resource_url: answer.resource_url,
+      platform_id: answer.platform_id,
+      resource_link_id: answer.resource_link_id,
+      platform_user_id: answer.platform_user_id
+    };
+  } else if (answer.resource_url && answer.run_key) {
+    return {
+      resource_url: answer.resource_url,
+      run_key: answer.run_key
+    };
+  }
+  return null;
 }
