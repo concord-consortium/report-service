b282e4ec3d724521e7677d931730746a8acbf0c2
eireland <eireland@concord.org>
2021-05-12 20:55:55 -0700

    Updated with to treat logged in users separately from anonymous users. Adds platform_id  sort

--- scripts/find-duplicate-answer.js
+++ scripts/find-duplicate-answer.js
@@ -8,90 +8,101 @@ if (!fs.existsSync(process.env.GOOGLE_APPLICATION_CREDENTIALS)) {
   process.exit(1);
 }
 
-const getQuestionKey = (questionUser) => {
-  return question = questionUser.split("#")[1].split("@")[0];
-}
-
 const firestore = new Firestore();
 const sourceCollection = "activity-player.concord.org"
 const answersRef = firestore.collection(`sources/${sourceCollection}/answers`)
-const resourceRef = firestore.collection(`sources/${sourceCollection}/resources`)
 let answerArr = [];
-let questionArr = [];
-let resourceArr = [];
 let questionUserMap = new Map;
 
-answersRef.orderBy("platform_user_id", "asc").orderBy("question_id", "asc").get().then(answerSnapshot => {
-  let prevData = {};
-  let prevDataKey = "";
-  let count = 0;
-  answerSnapshot.forEach(document => {
-    if (document.exists) {
-      let data = document.data();
-      let userId = data.platform_user_id;
-      let questionId = data.question_id;
-      let resourceLinkId = data.resource_link_id ? data.resource_link_id : `anonymous`;
-      let dataKey = (userId && questionId && resourceLinkId) ? `${userId}#${questionId}@${resourceLinkId}` : undefined;
+//finds logged in users
+const getLoggedInUsers = () => {
+  answersRef
+  .orderBy("platform_id", "asc")
+  .orderBy("resource_link_id", "asc")
+  .orderBy("platform_user_id", "asc")
+  .orderBy("question_id", "asc")
+  .orderBy("id", "asc")
+  .get()
+  .then(answerSnapshot => {
+    let prevData = {};
+    let prevDataKey = "";
+    let count = 0;
+    answerSnapshot.forEach(document => {
+      if (document.exists) {
+        let data = document.data();
+        let questionId = data.question_id;
+        let userId = data.platform_user_id;
+        let resourceLinkId = data.resource_link_id;
+        let platformId = data.platform_id;
+        let dataKey = (userId && questionId && resourceLinkId) ? `${platformId}/${resourceLinkId}&${userId}@${questionId}` : undefined;
 
-      if (dataKey === prevDataKey) {
+        if (dataKey === prevDataKey) {
           if (dataKey && questionUserMap.has(dataKey)) {
+            // console.log(dataKey, "already exists. This data will be deleted: ", data.id);
             answerArr = questionUserMap.get(dataKey)
-            if (!answerArr.includes(prevData.id)) {
-              answerArr.push(prevData.id)
+            if (!answerArr.includes(data.id)) {
+              answerArr.push(data.id)
             }
           } else {
-            questionUserMap.set(prevDataKey, [prevData.id]);
-          }
-      } else {
-        if (prevDataKey && questionUserMap.has(prevDataKey))
-          answerArr = questionUserMap.get(prevDataKey)
-          if (!answerArr.includes(prevData.id)) {
-            answerArr.push(prevData.id)
+            questionUserMap.set(prevDataKey, [prevData.id, data.id]);
           }
         }
-      }
-
-      prevData = data;
-      prevDataKey = dataKey;
-
-    } else {
-      console.error("No answer found with that id!")
-    }
-    count++;
-  })
-
-  for (let [questionUser] of questionUserMap) {
-    let questionId = questionUser && getQuestionKey(questionUser)
-    if (!questionArr.includes(questionId)) {
-      questionArr.push(questionId)
-    }
-  }
-
-  resourceRef.get().then(resourceSnapshot => {
-    resourceSnapshot.forEach(resource => {
-      if (resource.exists) {
-        let activity = resource.data()
-        let pages = activity.children
-        pages.forEach(page => {
-          let sections = page.children
-          sections.forEach(section => {
-            let parts = section.children
-            parts.forEach(part => {
-              if (!part.id.includes("page")) {
-                if (questionArr.includes(part.id) && !resourceArr.includes(part.id)) {
-                  resourceArr.push(resource)
-                }
-              }
-            })
-          })
-        })
+        prevData = data;
+        prevDataKey = dataKey;
+        count++;
+      } else {
+        console.error("No answer found with that id!")
       }
     })
+    console.log(questionUserMap.size)
+    // var file = fs.createWriteStream("multipleAnswerStudents.txt")
+    // questionUserMap.forEach(item => { file.write(item + ", \n") })
+    // file.end()
   })
-  console.log(questionUserMap)
-  var file = fs.createWriteStream("multipleAnswerStudents.txt")
-  questionUserMap.forEach(item => { file.write(item + ", \n") })
-  file.end()
+}
 
-})
+const getAnonymousUsers = () => {
+  answersRef
+  .orderBy("run_key", "asc")
+  .orderBy("question_id", "asc")
+  .get()
+  .then(answerSnapshot => {
+    let prevData = {};
+    let prevDataKey = "";
+    let count = 0;
+    answerSnapshot.forEach(document => {
+      if (document.exists) {
+        let data = document.data();
+        let questionId = data.question_id;
+        let runKey = data.run_key;
+        let dataKey = runKey && questionId ? `${runKey}@${questionId}` : `${data.resource_link_id}&${data.platform_user_id}@${data.question_id}`;
+
+        if ((!runKey=="") && (dataKey === prevDataKey)) {
+          if (dataKey && questionUserMap.has(dataKey)) {
+            // console.log(dataKey, "already exists. This data will be deleted: ", data.id);
+            answerArr = questionUserMap.get(dataKey)
+            if (!answerArr.includes(data.id)) {
+              answerArr.push(data.id)
+            }
+          } else {
+            questionUserMap.set(prevDataKey, [prevData.id, data.id]);
+          }
+        }
+        prevData = data;
+        prevDataKey = dataKey;
+        count++;
+      } else {
+        console.error("No answer found with that id!")
+      }
+    })
+    console.log(questionUserMap)
+    console.log(questionUserMap.size)
+    // var file = fs.createWriteStream("multipleAnswerStudents.txt")
+    // questionUserMap.forEach(item => { file.write(item + ", \n") })
+    // file.end()
+  })
+}
+
+getLoggedInUsers();
+// getAnonymousUsers();
 
