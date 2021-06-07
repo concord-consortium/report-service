326f4f190be15bb0dd78efd68cc214a4f4bee776
mklewandowski <matt.kain.lewandowski@gmail.com>
2021-06-07 15:47:03 -0700

    add error comment, remove token from url

--- results-loader/src/utils/results-utils.ts
+++ results-loader/src/utils/results-utils.ts
@@ -22,10 +22,24 @@ export const authorizeInPortal = (portalUrl: string, oauthClientName: string) =>
 };
 
 export const readPortalAccessToken = (portalUrl: string, oauthClientName: string): string => {
-  // No error handling to keep the code minimal.
+  // TODO: add error handling
+  // from PR: some error condition happens in the portal and it redirects back to the client with an error message.
+  // This error redirect can happen here:
+  // https://github.com/concord-consortium/rigse/blob/917d5aaa788502b00fe906749af80275203b83fd/rails/app/controllers/auth_controller.rb#L45-L47
+  // The invalid response can happen for these 2 reasons:
+  // https://github.com/concord-consortium/rigse/blob/917d5aaa788502b00fe906749af80275203b83fd/rails/app/models/access_grant.rb#L65-L74
+  // Basically if the client is not configured correctly in the portal. Or the request from the client uses an invalid response type.
+  // The response type is handled by the ClientOAuth2 library so hopefully that would never be a problem. But if the client is not configured with the
+  // 'public' type then the portal should redirect back to the client with a error url param.
   const accessToken = getURLParam("access_token");
   if (!accessToken) {
     authorizeInPortal(portalUrl, oauthClientName);
+  } else {
+    // c.f. https://stackoverflow.com/questions/22753052/remove-url-parameters-without-refreshing-page
+    if (window.history?.pushState !== undefined) {
+      // if pushstate exists, add a new state to the history, this changes the url without reloading the page
+      window.history.pushState({}, document.title, window.location.pathname);
+    }
   }
   return accessToken || "";
 };
