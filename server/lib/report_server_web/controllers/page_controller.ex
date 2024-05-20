86e623bfb2eec7a0af7fc0a547908fe114752cbf
Doug Martin <dmartin@concord.org>
2024-05-20 16:05:21 -0400

    Added /config endpoint to expose non-secret configuration options
    I needed to add this to figure out why the production server wasn't walking to the production token service.  You'll see the fix for that in TokenService#get_env.

--- server/lib/report_server_web/controllers/page_controller.ex
+++ server/lib/report_server_web/controllers/page_controller.ex
@@ -1,9 +1,35 @@
 defmodule ReportServerWeb.PageController do
   use ReportServerWeb, :controller
 
+  alias ReportServerWeb.{Auth, TokenService}
+
+
   def home(conn, _params) do
     # The home page is often custom made,
     # so skip the default app layout.
     render(conn, :home, page_title: "Home")
+  end
+
+  def config(conn, _params) do
+    session = get_session(conn)
+    if Auth.logged_in?(session) do
+      portal = Application.get_env(:report_server, :portal) |> Enum.into(%{})
+      token_service = Application.get_env(:report_server, :token_service) |> Enum.into(%{})
+      output = Application.get_env(:report_server, :output) |> Enum.into(%{})
+
+      portal_credentials = Auth.get_portal_credentials(session)
+      {:ok, token_service_env} = TokenService.get_env("prod", portal_credentials)
+
+      config = %{
+        portal: portal,
+        token_service: token_service,
+        output: output,
+        token_service_env: token_service_env,
+        full_token_service_url: TokenService.get_token_service_url(token_service_env)
+      }
+      render(conn, :config, page_title: "Config", config: config, error: nil)
+    else
+      render(conn, :config, page_title: "Config", error: "To get the config you first need to login.")
+    end
   end
 end
