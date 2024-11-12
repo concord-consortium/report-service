82990d028cbf77baf79f1e045b7402c7ea0059c5
Boris Goldowsky <bgoldowsky@concord.org>
2024-11-12 10:57:40 -0500

    Start of resource details query

--- server/lib/report_server/reports/resource_metrics_details.ex
+++ server/lib/report_server/reports/resource_metrics_details.ex
@@ -1,5 +1,5 @@
 defmodule ReportServer.Reports.ResourceMetricsDetails do
-
+  alias ReportServer.PortalDbs
   alias ReportServer.Reports.Report
 
   def new() do
@@ -8,13 +8,49 @@ defmodule ReportServer.Reports.ResourceMetricsDetails do
       title: "Resource Metrics Details",
       subtitle: "Detail report on resource metrics",
       filters: [ "resource" ],
-      run: &ReportServer.Reports.TeacherStatus.run/1 # &run/1
+      run: &run/1 # &run/1
     }
   end
 
-  # add this back when not delegating to the teacher status report for initial development
-  # def run(filters) do
-  #   IO.inspect(filters, label: "Running #{__MODULE__}")
-  # end
+  def run(filters) do
+    IO.inspect(filters, label: "Running #{__MODULE__}")
+    dev_query_portal = "learn.concord.org" # FIXME
+    dev_query = """
+    select
+      trim(ea.name) as activity_name,
+      rl.teachers_name as teacher_name,
+      rl.teachers_email as teacher_email,
+      rl.school_id as school_id,
+      rl.school_name as school_name,
+      rl.teachers_district as school_district,
+      rl.teachers_state as school_state,
+      count(distinct rl.class_id) as number_of_classes,
+      count(distinct rl.student_id) as number_of_students
+    from
+      external_activities ea
+      left join report_learners rl on (rl.runnable_id = ea.id and rl.last_run is not null)
+    where
+      ea.id between 1 and 20
+    group by
+      ea.id, rl.teachers_name, rl.teachers_email, rl.school_id
+    order by
+      ea.name, rl.teachers_name
+    """
+    PortalDbs.query(dev_query_portal, dev_query)
+  end
+
+  # Activity
+  # Teacher Name
+  # Teacher Email
+  # School name
+  # School country -- ?
+  # School state
+  # Grade level -- ?
+  # Number of classes
+  # Number of students
+  # Date first assigned - TODO
+  # Date first student use - TODO
+  # Date last assigned - TODO
+  # Date last student use - TODO
 
 end
