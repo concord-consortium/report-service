2870c72dc9ea926fbe3965e1abb34483707b7702
Doug Martin <dmartin@concord.org>
2024-11-14 05:52:09 -0500

    feat: Added canned resource metrics summary report [PT-188527972]

--- server/lib/report_server/reports/resource_metrics_summary.ex
+++ server/lib/report_server/reports/resource_metrics_summary.ex
@@ -1,12 +1,31 @@
 defmodule ReportServer.Reports.ResourceMetricsSummary do
 
+  alias ReportServer.PortalDbs
   alias ReportServer.Reports.Report
+  alias ReportServer.Reports.ReportFilter
 
-  def new(report = %Report{}), do: %{report | run: &ReportServer.Reports.TeacherStatus.run/1 } # &run/1
+  def new(report = %Report{}), do: %{report | run: &run/1 }
 
-  # add this back when not delegating to the teacher status report for initial development
-  # def run(filters) do
-  #   IO.inspect(filters, label: "Running #{__MODULE__}")
-  # end
+  def run(_filters) do
+    dev_query_portal = "learn.concord.org"
+    dev_query = """
+    select
+      trim(ea.name) as activity_name,
+      count(distinct rl.teachers_id) as number_of_teachers,
+      count(distinct rl.school_id) as number_of_schools,
+      count(distinct rl.class_id) as number_of_classes,
+      count(distinct rl.id) as number_of_students
+    from
+      external_activities ea
+      left join report_learners rl on (rl.runnable_id = ea.id and rl.last_run is not null)
+    where
+      ea.id in (2304, 3091)
+    group by
+      ea.id
+    order by
+      ea.name
+    """
+    PortalDbs.query(dev_query_portal, dev_query)
+  end
 
 end
