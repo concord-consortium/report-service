540f10f182f8857b04b17750e36be02e9fc04b8f
Doug Martin <dmartin@concord.org>
2024-11-14 06:13:20 -0500

    feat: Added canned resource metrics details report [PT-188527975]

--- server/lib/report_server/reports/resource_metrics_details.ex
+++ server/lib/report_server/reports/resource_metrics_details.ex
@@ -1,12 +1,40 @@
 defmodule ReportServer.Reports.ResourceMetricsDetails do
-
+  alias ReportServer.PortalDbs
   alias ReportServer.Reports.Report
+  alias ReportServer.Reports.ReportFilter
 
-  def new(report = %Report{}), do: %{report | run: &ReportServer.Reports.TeacherStatus.run/1 } # &run/1
+  def new(report = %Report{}), do: %{report | run: &run/1 }
 
-  # add this back when not delegating to the teacher status report for initial development
-  # def run(filters) do
-  #   IO.inspect(filters, label: "Running #{__MODULE__}")
-  # end
+  def run(_filters) do
+    dev_query_portal = "learn.concord.org"
+    dev_query = """
+    select
+      trim(ea.name) as activity_name,
+      rl.teachers_name as teacher_name,
+      rl.teachers_email as teacher_email,
+      rl.school_name as school_name,
+      'TBD' as school_country,
+      rl.teachers_state as school_state,
+      rl.teachers_district as school_district,
+      count(distinct rl.class_id) as number_of_classes,
+      'TBD' as grade_levels,
+      count(distinct rl.student_id) as number_of_students,
+      'TBD' as assigned,
+      'TBD' as first_student_use,
+      'TBD' as most_recent_student_use
+    from
+      external_activities ea
+      join report_learners rl on (rl.runnable_id = ea.id and rl.last_run is not null)
+      join portal_schools ps on (ps.id = rl.school_id)
+      join portal_teachers pt on (pt.id = rl.teachers_id)
+    where
+      ea.id in (2304, 3091)
+    group by
+      ea.id, rl.teachers_name, rl.teachers_email, rl.school_id
+    order by
+      ea.name, rl.teachers_name
+    """
+    PortalDbs.query(dev_query_portal, dev_query)
+  end
 
 end
