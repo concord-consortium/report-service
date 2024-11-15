303696847b9116c788fc9aea56b16fc291ec509f
Doug Martin <dmartin@concord.org>
2024-11-15 05:39:47 -0500

    feat: Updated styling of filter form [PT-188527665]

--- server/lib/report_server_web/live/new_report_live/form.ex
+++ server/lib/report_server_web/live/new_report_live/form.ex
@@ -15,7 +15,7 @@ defmodule ReportServerWeb.NewReportLive.Form do
   alias ReportServer.Reports
   alias ReportServer.Reports.{Report, Tree}
 
-  @filter_type_options [{"Select a filter...", ""}, {"Schools", "school"}, {"Cohorts", "cohort"}, {"Teachers", "teacher"}, {"Assignments", "assignment"}]
+  @filter_type_options [{"Schools", "school"}, {"Cohorts", "cohort"}, {"Teachers", "teacher"}, {"Assignments", "assignment"}]
 
   @impl true
   def mount(_params, _session, socket) do
